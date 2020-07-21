8b2444ace8ab955e45534980c7e7741b8e7cc00f
Alex Suraci <suraci.alex@gmail.com>
2020-07-21 15:08:30 -0400

    commit with great skepticism
    Signed-off-by: Alex Suraci <suraci.alex@gmail.com>

--- main.py
+++ main.py
@@ -11,13 +11,18 @@ from github import Github
 def get_prs(repo, pivotal_members, show_pivots):
     pr_count = defaultdict(int)
 
-    for prs in repo.get_pulls('all'):
-
-        if is_pivot(prs.user.login, pivotal_members) and not show_pivots:
+    for pr in repo.get_pulls('all'):
+        if not pr.merged:
             continue
 
-        created = prs.created_at
-        created = created.replace(hour=0, minute=0, second=0, microsecond=0) # squash it down to the date
+        if pr.user.login.endswith('[bot]'):
+            continue
+
+        if is_pivot(pr.user.login, pivotal_members) and not show_pivots:
+            continue
+
+        created = pr.created_at.replace(hour=0, minute=0, second=0, microsecond=0) # squash it down to the date
+
         pr_count[created] += 1
 
     dates, occurances = list(pr_count.keys()), list(pr_count.values())
@@ -29,23 +34,22 @@ def is_pivot(login, pivotal_member):
     for member in pivotal_member:
         if login == member.login:
             return True
-        if login == 'dependabot':
+        if login in ['deniseyu', 'xtreme-james-cooper', 'mhuangpivotal', 'julia-pu']:
             return True
 
-    return False
+    print("Non-pivot: {}".format(login))
 
-def show_pivots(opts): 
-    return opts.show_pivots
+    return False
 
 def process_arguments():
     parser = argparse.ArgumentParser(description='Process a git repo and graph the PRs. See README.md for setup instructions.')
     parser.add_argument('-sp', '--show-pivots', action='store_true', help='include pivotal contributions')
 
     args = parser.parse_args()
-    
+
     return args
 
-def print_results(monthly, yearly): 
+def print_results(monthly, yearly):
     monthlySum = monthly.sum()
     yearlySum = yearly.sum()
 
@@ -56,29 +60,29 @@ def print_results(monthly, yearly):
     print(yearlySum.to_string())
 
 if __name__ == "__main__":
-
     cli_arguments = process_arguments()
-    show_pivots = show_pivots(cli_arguments)
-    
+
     g = Github(settings.GH_KEY)
     org = g.get_organization(settings.ORGANIZATION)
-    
-    pivotal_members = []
-
-    for team in org.get_teams():
-        if team.name == 'Pivotal':
-            pivotal_members = [member for member in team.get_members()]
-
-    repos = [org.get_repo(x) for x in settings.REPOS]
 
     print("Scanning data in {}".format(settings.ORGANIZATION))
 
+    pivotal_members = []
+
+    for team in org.get_teams():
+        if team.name == 'Pivotal' or team.name == 'Alumni':
+            pivotal_members += [member for member in team.get_members()]
+
+    repos = [org.get_repo(x) for x in settings.REPOS]
+
+    print("Members: {}".format(pivotal_members))
+
     totals_dict = defaultdict(int)
-    
+
     for repo in repos:
         print("Scanning {}".format(repo.full_name))
 
-        array_dates, array_occurances, pr_counts = get_prs(repo, pivotal_members, show_pivots)
+        array_dates, array_occurances, pr_counts = get_prs(repo, pivotal_members, cli_arguments.show_pivots)
 
         for key, value in pr_counts.items():
             totals_dict[key] += value
@@ -88,9 +92,9 @@ if __name__ == "__main__":
     df = df.set_index(df['Date'])
     df.index = pd.to_datetime(df.index)
 
-    monthly = df.groupby(pd.Grouper(freq="M")) 
+    monthly = df.groupby(pd.Grouper(freq="M"))
     yearly  = df.groupby(pd.Grouper(freq="Y"))
-    
+
     print_results(monthly, yearly)
 
     plt.figure(figsize=(12, 16), dpi=600)
@@ -99,7 +103,7 @@ if __name__ == "__main__":
     ax = yearly.sum().plot.line()
 
     ax.set_title("# of community Concourse PRs over time")
-    ax.set_ylabel("# PRs per month")
+    ax.set_ylabel("# PRs per year")
     ax.set_xlabel("Date")
     ax.grid()
 
